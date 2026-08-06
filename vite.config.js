ad36c0bdf88ed4c62fc525c13447feabc7176d06
Tania S. Sainz de Aja <tssainzdeaja@gmail.com>
2026-08-06 15:20:33 +0200

    change name repo

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
-  plugins: [react()],
-  server: {
-    open: "/",
-    watch: {
-      usePolling: true,
+    base: "./tania-portafolio",
+    plugins: [react()],
+    server: {
+        open: "/",
+        watch: {
+            usePolling: true,
+        },
     },
-  },
 });
